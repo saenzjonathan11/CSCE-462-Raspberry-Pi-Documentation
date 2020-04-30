690998fa405001c609298580684be4b699b1ce98
saenzjonathan11 <saenzjonathan11@gmail.com>
2020-04-30 00:52:56 -0500

    working on dashboard

--- pi-dashboard/status/urls.py
+++ pi-dashboard/status/urls.py
@@ -8,13 +8,10 @@ app_name = 'status'
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.dashboard, name='dashboard'),
-    # path('dashboard/', views.dashboard, name='dashboard'),
     path('hotspot/', views.hotspot, name='hotspot'),
     path('interfaces/', views.interfaces, name='interfaces'),
     path('dhcp_dns/', views.dhcp_dns, name='dhcp_dns'),
     path('system/', views.system, name='system'),
-    path('authentication/', views.authentication, name='authentication'),
-    path('about/', views.about, name='about'),
-    path('devices/', views.devices, name='devices'),
-    path('device/<int:device_num>/', views.device, name='device')
+    # path('authentication/', views.authentication, name='authentication'),
+    path('about/', views.about, name='about')
 ]
--- pi-dashboard/status/views.py
+++ pi-dashboard/status/views.py
@@ -2,18 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 import os
+import re
 
 hostap_status = os.popen('pidof hostapd | wc -l').readline()
 cpu_temp = os.popen('vcgencmd measure_temp').readline()
 mem_free = os.popen("free -m | awk '/Mem:/ {total=$2; used=$3} END { print used/total*100}'").readline()
-#mem_free = os.popen("cat /proc/meminfo | awk 'FNR == 3 {free=$2}' END '{pring free/2000000*100}'").readline()
 rx_packets = os.popen('cat /sys/class/net/wlan0/statistics/rx_packets').readline()
 tx_packets = os.popen('cat /sys/class/net/wlan0/statistics/tx_packets').readline()
 tx_bytes = os.popen('cat /sys/class/net/wlan0/statistics/tx_bytes').readline()
 rx_bytes = os.popen('cat /sys/class/net/wlan0/statistics/rx_bytes').readline()
 iw_wlan0 = os.popen('iw dev wlan0 info').readlines()
 devices = os.popen('cat /var/lib/misc/dnsmasq.leases').readlines()
-
 
 system_infos = [
         {
@@ -29,21 +28,15 @@ system_infos = [
         }
 ]
 
-posts = [
-    {
-        'hostname': 'dummylocalhost',
-        'ip4_address': '127.0.0.1',
-        'ip6_address': '0:0:0:0:0:0:0:1',
-        'mac_address': '05:32:E3:01:CC:D4',
-        'bandw_in': 12,
-        'bandw_out': 30, 
-        'bandw_total': 30,
-        'last_seen': 'Mon 06 Apr 2020 02:49:45 AM CDT'
-    }
-]
+wifiStandards = {
+        'a': '802.11a - 5 GHz',
+        'b': '802.11b - 2.4 GHz',
+        'g': '802.11g - 2.4 GHz',
+        'n': '802.11n - 2.4 GHz',
+        'ac': '802.11.ac - 5 GHz'
+        }
 
 def index(request):
-    # interfacesList = [x.strip() for x in os.popen('ls /sys/class/net | grep -v lo').readlines()]
     interfacesList = [x.strip() for x in os.popen('ls /sys/class/net').readlines()]
     ifconfigOut = os.popen('ifconfig').readlines()
     tmpInterface = ""
@@ -58,26 +51,51 @@ def index(request):
     zipInterfaces = zip(interfacesList,interfacesStrs)
 
     context = {
-        'posts': posts,
         'systemInfos': system_infos,
         'interfacesList': interfacesList,
         'zipInterfaces': zipInterfaces
-        # 'interfacesStrs': interfacesStrs,
-        # 'interfacesList': interfacesList
     }
     return render(request, 'status/index.html',context)
-    #return render(request, 'status/index.html', context)
 
 def dashboard(request):
+    devices = os.popen('ifconfig').readlines()
+
     return render(request, 'status/dashboard.html')
 
 def hotspot(request):
 
-    
-    return render(request, 'status/hotspot.html')
+    hostapdConf = [x.strip() for x in os.popen('cat /etc/hostapd/hostapd.conf').readlines()]
 
+    ap_interface=""
+    ap_ssid = ""
+    ap_country_code = ""
+    ap_channel = ""
+    ap_passphrase = ""
+    ap_hw_mode = ""
 
+    for hostapdCon in hostapdConf:
+        if (re.search('(?<=^interface=).*', hostapdCon)):
+            ap_interface = re.search('(?<=^interface=).*', hostapdCon).group()
+        if (re.search('(?<=^ssid=).*', hostapdCon)):
+            ap_ssid = re.search('(?<=^ssid=).*', hostapdCon).group()
+        if (re.search('(?<=^country_code=).*', hostapdCon)):
+            ap_country_code = re.search('(?<=^country_code=).*', hostapdCon).group()
+        if (re.search('(?<=^channel=).*', hostapdCon)):
+            ap_channel = re.search('(?<=^channel=).*', hostapdCon).group()
+        if (re.search('(?<=^wpa_passphrase=).*', hostapdCon)):
+            ap_passphrase = re.search('(?<=^wpa_passphrase=).*', hostapdCon).group()
+        if (re.search('(?<=^hw_mode=).*', hostapdCon)):
+            ap_hw_mode = re.search('(?<=^hw_mode=).*', hostapdCon).group()
 
+    context = {
+        'apInterface': ap_interface,
+        'apSSID': ap_ssid,
+        'apChannel': ap_channel,
+        'apPassphrase': ap_passphrase,
+        'apHwMode': wifiStandards[ap_hw_mode],
+        'apCountryCode': "US"
+    }
+    return render(request, 'status/hotspot.html',context)
 
 def interfaces(request):
     interfacesList = [x.strip() for x in os.popen('ls /sys/class/net').readlines()]
@@ -94,33 +112,20 @@ def interfaces(request):
     zipInterfaces = zip(interfacesList,interfacesStrs)
 
     context = {
-        'posts': posts,
         'systemInfos': system_infos,
         'interfacesList': interfacesList,
         'zipInterfaces': zipInterfaces
     }
     return render(request, 'status/interfaces.html', context)
 
-
 def dhcp_dns(request):
     return render(request, 'status/dhcp_dns.html')
 
-def authentication(request):
-    return render(request, 'status/system.html')
+# def authentication(request):
+#     return render(request, 'status/system.html')
 
 def system(request):
     return render(request, 'status/system.html')
 
 def about(request):
     return render(request, 'status/about.html')
-
-def device(request, device_num):
-    response = "You're looking at the result of device %s."
-    return HttpResponse(response % device_num) 
-
-def devices(request):
-    context = {
-        'posts': posts
-    }
-    return render(request, 'status/devices.html', context)
-
